24b71e281caefc9db430f4c007bf479fe07b9a6c
Cedric Matheus <cedric.ms.alves@gmail.com>
2023-11-07 14:44:49 -0300

    0.9.0 - Create Theme Model

---
+++ src/models/theme.js
@@ -0,0 +1,8 @@
+/**
+ * @typedef Theme
+ * @type {object}
+ * @property {string} backgroundColor - Cor do fundo (#ffffff).
+ * @property {string} textColor - Cor do texto (#000000).
+ */
+
+export {};
