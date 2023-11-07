992e7f693103443c7d0e5be6d6c80c618c77cc10
Cedric Matheus <cedric.ms.alves@gmail.com>
2023-11-07 10:37:43 -0300

    0.3.0 - Create User Model

---
+++ src/models/user.js
@@ -0,0 +1,9 @@
+/**
+ * @typedef User
+ * @type {object}
+ * @property {number} weight - Peso atual do usuário.
+ * @property {number} dailyPhysicalActivity - Nível de atividade física diária.
+ * @property {number[]} totalWaterRange - Faixa de água necessária do usuário (0=min, 1=max).
+ */
+
+export {};
