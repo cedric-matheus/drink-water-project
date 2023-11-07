fac78bcd4844214fac34e479d834a2e0a19b83a9
Cedric Matheus <cedric.ms.alves@gmail.com>
2023-11-07 17:08:09 -0300

    0.18.0 - Add Current Day Events

--- src/index.js
+++ src/index.js
@@ -1,11 +1,14 @@
 import UserService from './services/user.js';
 import ThemeService from './services/theme.js';
+import DayService from './services/day.js';
 
 const userDataView = document.getElementById("userData");
 const themeView = document.getElementById("theme");
+const currentDayView = document.getElementById("currentDay");
 
 const userForm = document.getElementById("userDataForm");
 const themeForm = document.getElementById("themeForm");
+const drinkWaterForm = document.getElementById("drinkWaterForm");
 
 function setTheme() {
   const themeData = ThemeService.getThemeData();
@@ -25,9 +28,28 @@ function setTheme() {
   }
 }
 
+function refreshCurrentDayViewData() {
+  const currentDayData = DayService.getCurrentDayData();
+
+  const dateElement = document.getElementById("currentDayDate");
+  const totalWaterElement = document.getElementById("currentDayTotalWater");
+  const drunkWaterDifferenceElement = document.getElementById("currentDayDrunkWaterDifference");
+  const totalDrunkWaterElement = document.getElementById("currentDayTotalDrunkWater");
+
+  const totalWaterText = `Total de água no dia: ${currentDayData.totalObjectiveWater[0]}ml a ${currentDayData.totalObjectiveWater[1]}ml`;
+  const drunkWaterDifferenceText = `Total de água restante: ${currentDayData.waterDrunkDifference[0]}ml a ${currentDayData.waterDrunkDifference[1]}ml`;
+  const totalDrunkWaterText = `Total de água ingerido: ${currentDayData.totalWaterDrunk}ml`;
+
+  dateElement.innerText = currentDayData.date;
+  totalWaterElement.innerText = totalWaterText;
+  drunkWaterDifferenceElement.innerText = drunkWaterDifferenceText;
+  totalDrunkWaterElement.innerText = totalDrunkWaterText;
+}
+
 function hiddenViews() {
   userDataView.style.display = "none";
   themeView.style.display = "none";
+  currentDayView.style.display = "none";
 }
 
 function refreshViews() {
@@ -39,7 +61,22 @@ function refreshViews() {
     themeView.style.display = "block";
   else {
     setTheme();
+    refreshCurrentDayViewData();
+    currentDayView.style.display = "block"
   }
+}
+
+function startNewDay() {
+  const userData = UserService.getUserData();
+  DayService.setTotalObjectiveWater(userData.totalWaterRange)
+}
+
+function startApp() {
+  const userData = UserService.getUserData();
+  const currentDayData = DayService.getCurrentDayData();
+
+  if(userData.totalWaterRange !== undefined && currentDayData.totalObjectiveWater === undefined)
+    startNewDay();
 }
 
 function submitUserForm(event){
@@ -52,6 +89,7 @@ function submitUserForm(event){
   UserService.setUserDailyPhysicalActivity(dataObject.userDailyPhysicalActivity);
   UserService.calculateTotalWaterRange();
 
+  startNewDay();
   refreshViews();
 }
 
@@ -70,8 +108,20 @@ function submitThemeForm(event){
   refreshViews();
 }
 
+function submitDrinkWaterForm(event){
+  event.preventDefault();
+
+  const data = new FormData(event.target);
+  const dataObject = Object.fromEntries(data.entries());
+
+  DayService.drinkWater(parseInt(dataObject.totalWaterDrunk));
+
+  refreshViews();
+}
+
 userForm.addEventListener("submit", submitUserForm);
 themeForm.addEventListener("submit", submitThemeForm);
+drinkWaterForm.addEventListener("submit", submitDrinkWaterForm);
 
+startApp();
 refreshViews();
-
