c0a2380c997d128ed7dc31d07cda86b66b9dc723
Cedric Matheus <cedric.ms.alves@gmail.com>
2023-11-07 11:00:04 -0300

    0.5.0 - Create Day Model

---
+++ src/models/day.js
@@ -0,0 +1,10 @@
+/**
+ * @typedef Day
+ * @type {object}
+ * @property {string} date - Data correspondente ao dia.
+ * @property {number[]} totalObjectiveWater - Total de água do dia (0=min e 1=max).
+ * @property {number} totalWaterDrunk - Total de água ingerida.
+ * @property {number[]} waterDrunkDifference - Diferença entre água ingerida e objetivo (0=min e 1=max).
+ */
+
+export {};
